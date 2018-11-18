67fb794889c1c3f18d19ffa9b5c16637faedfc5b
Michael Schuresko <michael.schuresko@gmail.com>
2018-11-18 13:57:28 -0800

    Initial commit

---
+++ Cipher.py
@@ -0,0 +1,39 @@
+from random import randrange
+import random
+
+def permute(in_str):
+    a = [l for l in in_str]
+    n = len(a)
+    for i in range(n):
+        idx = randrange(i, n)
+        tmp = a[i]
+        a[i] = a[idx]
+        a[idx] = tmp
+    return "".join(a)
+
+class Cipher:
+    def __init__(self, perm=None):
+        self.letters = 'abcdefghijklmnopqrstuvwxyz'
+        if perm:
+            self.perm = perm
+        else:
+            self.perm = permute(self.letters)
+        self.f_map = {} # forward map
+        self.r_map = {} # reverse map
+        for i in range(26):
+            self.f_map[self.letters[i]] = self.perm[i]
+            self.r_map[self.perm[i]] = self.letters[i]
+    def scramble(self, in_str):
+        s = [l for l in in_str.lower() if l in self.letters]
+        return "".join([self.f_map[l] for l in s])
+    def unscramble(self, in_str):
+        s = [l for l in in_str.lower() if l in self.letters]
+        return "".join([self.r_map[l] for l in s])
+    def mutate(self, cycle):
+        p = {}
+        for i in range(len(cycle)):
+            p[cycle[i-1]] = cycle[i]
+        nextperm = "".join([p.get(l, l) for l in self.perm])
+        return Cipher(nextperm)
+    
+        
---
+++ TriGram.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.corpus import brown
+
+class TriGram:
+    def __init__(self):
+        self.tri_map = {}
+        self.tri = ""
+        self.letters = 'abcdefghijklmnopqrstuvwxyz'
+        print("here")
+        for w in brown.words():
+            self.ingest(w)
+    def ingest(self, w):
+        for l in w.lower():
+            if not l in self.letters:
+                continue
+            self.tri = self.tri + l
+            self.tri = self.tri[-3:]
+            if len(self.tri) > 2:
+                self.tri_map[self.tri] = self.tri_map.get(self.tri, 0) + 1
+    def lookup(self, tri):
+        return self.tri_map.get(tri,0)
+    def freqs(self, in_str):
+        s = ''.join([l for l in in_str.lower() if l in self.letters])
+        n = len(s) - 2
+        tris = [s[i:][:3] for i in range(n)]
+        return [self.tri_map.get(tri, 0) for tri in tris]
+    
