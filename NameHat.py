e0ff67c05afc592163600fe3494bb1522b44f981
Michael Schuresko <michael.schuresko@gmail.com>
2020-05-12 07:22:30 -0700

    NameHat is the most 'useful' piece of this repo.  I tweaked that

--- NameHat.py
+++ NameHat.py
@@ -4,18 +4,34 @@
 from CryptoGibbs import *
 import random
 
-def make_name(len_range=(6,10)):
-    n = random.randrange(len_range[0], len_range[1])
+def make_names(words=2, len_range=(6,10)):
+    sentence = ""
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    l = [letters[random.randrange(26)] for i in range(n)]
-    w = " " + "".join(l) + " "
-    c = CryptoGibbs(w)
+    for i in range(words):
+        n = random.randrange(len_range[0], len_range[1])
+        l = [letters[random.randrange(26)] for i in range(n)]
+        sentence = sentence + " " + "".join(l)
+    sentence = sentence + " "
+    c = CryptoGibbs(sentence)
     c.jump_for(10000)
-    return c.c.unscramble(c.s)
+    return c.c.unscramble(c.s).strip().split(" ")
 
 if __name__ == "__main__":
+    from sys import argv
     print("Calculating names")
-    names = [make_name((7,12)) for i in range(20)]
+    names = []
+    sentences = 25
+    words_per_sentence = 1
+    try:
+        num_words = int(argv[-1])
+        words_per_sentence = num_words
+        sentences = int(sentences/num_words)
+    except:
+        print("Defaulting to " + str(words_per_sentence) +
+              " words per sentence and " + str(sentences) +
+              " sentences")
+    for i in range(sentences):
+        names = names + make_names(words=words_per_sentence, len_range=(7,12))
     print("printing the names")
     [print(n) for n in names]
 
